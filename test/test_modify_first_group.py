a65240e1bb997dfeb4d0ba94c0c145143732129c
Lucas1985 <lukkba@gmail.com>
2017-09-24 00:28:21 +0200

    working test_modify_gfirst_group

--- test/test_modify_first_group.py
+++ test/test_modify_first_group.py
@@ -4,10 +4,10 @@ from model.group import Group
 
 def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Group(name="New name", header="edit", footer="edit"))
+    app.group.modify_first_group(Group(name="New name"))
     app.session.logout()
 
-#def test_modify_group_header(app):
-#    app.session.login(username="admin", password="secret")
-#    app.group.modify_first_group(Group(name="New header", header="edit", footer="edit"))
-#    app.session.logout()
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New header"))
+    app.session.logout()
