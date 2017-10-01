200ef89eb61edb37184abf2ae172c82fd925a4c2
Lucas1985 <lukkba@gmail.com>
2017-10-01 17:54:08 +0200

    comparing lists func added

--- test/test_modify_first_group.py
+++ test/test_modify_first_group.py
@@ -5,9 +5,16 @@ from model.group import Group
 def test_modify_group_name(app):
     if app.group.count_of_groups() == 0:
         app.group.create(Group(name="New group to modify"))
-    app.group.modify_first_group(Group(name="New name"))
+    old_groups = app.group.get_group_list()
+    app.group.modify_first_group(Group(name="New group name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_group_header(app):
     if app.group.count_of_groups() == 0:
         app.group.create(Group(name="New group to modify"))
-    app.group.modify_first_group(Group(header="New header"))
+    old_groups = app.group.get_group_list()
+    app.group.modify_first_group(Group(header="New group header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+
