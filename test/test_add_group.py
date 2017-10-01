60693344aed9d70f35dac83acc373299ae428dd1
Lucas1985 <lukkba@gmail.com>
2017-10-01 19:22:59 +0200

    cache and hashe fixed

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,7 +6,7 @@ def test_add_group(app):
     old_groups = app.group.get_group_list()
     group = Group(name="nowa_grupa", header="aaaaa", footer="bbbbb")
     app.group.create(group)
-    assert len(old_groups) + 1 == len(app.group.count_of_groups)
+    assert len(old_groups) + 1 == app.group.count_of_groups()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
