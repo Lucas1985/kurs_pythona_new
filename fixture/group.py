941a68c8b9ea9ff1892f48de978d06bef30a714a
Lucas1985 <lukkba@gmail.com>
2017-10-01 20:56:59 +0200

    randomized test for groups

--- fixture/group.py
+++ fixture/group.py
@@ -33,22 +33,33 @@ class GroupHelper(CreatorHelper):
         self.change_field_value("group_footer", group.footer)
 
     def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_checkbox()
+        self.select_group_by_index(index)
         # submit group deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
         self.group_cache = None
 
+    def select_group_by_index(self, index):
+       wd = self.app.wd
+       wd.find_elements_by_name("selected[]")[index].click()
+
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
-    def modify_first_group(self, new_group_data):
+    def modify_first_group(self):
+        wd = self.app.wd
+        self.modify_group_by_index(0)
+
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_checkbox()
+        self.select_group_by_index(index)
         # Click edit group
         wd.find_element_by_name("edit").click()
         self.fill_group_form(new_group_data)
