33d96c6eeac22e5e9cb6e4805a9b41b176901b6e
Lucas1985 <lukkba@gmail.com>
2017-10-01 19:09:05 +0200

    cache and hashe

--- test/test_modify_first_group.py
+++ test/test_modify_first_group.py
@@ -6,9 +6,14 @@ def test_modify_group_name(app):
     if app.group.count_of_groups() == 0:
         app.group.create(Group(name="New group to modify"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="New group name"))
+    group = Group(name="New group name")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
 
 def test_modify_group_header(app):
     if app.group.count_of_groups() == 0:
